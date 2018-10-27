29961e43c2b6e5e4171727f438131dcff9071ff7
kawakubox <shigefumi.kawakubo@gmail.com>
2018-10-27 13:24:54 +0900

    [#10] Implements KSJUrl Api

--- lib/mlit_api/client.rb
+++ lib/mlit_api/client.rb
@@ -19,6 +19,16 @@ module MlitApi
       end
     end
 
+    def urls(**args)
+      params = args.slice(:identifier, :fiscalyear)
+      doc = REXML::Document.new url_contents(params)
+      REXML::XPath.match(doc, '//KSJ_URL/item').map do |node|
+        url_attributes.each_with_object({}) do |attr, h|
+          h[attr] = node.elements[attr.to_s]&.text
+        end
+      end
+    end
+
     private
 
     def summary_resource_path
@@ -39,6 +49,19 @@ module MlitApi
 
     def summary_attributes
       %i(identifier title field1 field2 areaType)
+    end
+
+    def url_resource_path
+      'ksj/api/1.0b/index.php/app/getKSJURL.xml'
+    end
+
+    def url_contents(**params)
+      base_params = { appId: 'ksjapibeta1', lang: 'J', dataformat: 1 }
+      @conn.get(url_resource_path, base_params.merge(params)).body
+    end
+
+    def url_attributes
+      %i(identifier title field year areaType areaCode datum zipFileUrl zipFileSize)
     end
   end
 end
--- spec/mlit_api/client_spec.rb
+++ spec/mlit_api/client_spec.rb
@@ -17,4 +17,26 @@ RSpec.describe MlitApi::Client do
     it { expect(client.summaries.first[:field2]).to eq '大都市圏' }
     it { expect(client.summaries.first[:areaType]).to eq '2' }
   end
+
+  describe :urls do
+    let(:id) { 'L01' }
+    let(:year) { 2018 }
+    let(:xml) { File.read('spec/fixtures/files/KSJUrl.xml') }
+
+    subject { client.urls(identifier: id, fiscalyear: year) }
+
+    before { allow(client).to receive(:url_contents) { xml } }
+
+    it { is_expected.to be_a Array }
+    it { expect(subject.length).to eq 48 }
+    it { expect(subject.first[:identifier]).to eq 'L01' }
+    it { expect(subject.first[:title]).to eq '地価公示' }
+    it { expect(subject.first[:field]).to eq '国土（水・土地）' }
+    it { expect(subject.first[:year]).to eq '2018' }
+    it { expect(subject.first[:areaType]).to eq '3' }
+    it { expect(subject.first[:areaCode]).to eq '0' }
+    it { expect(subject.first[:datum]).to eq '1' }
+    it { expect(subject.first[:zipFileUrl]).to eq 'http://nlftp.mlit.go.jp/ksj/gml/data/L01/L01-18/L01-18_GML.zip' }
+    it { expect(subject.first[:zipFileSize]).to eq '15.70MB' }
+  end
 end
